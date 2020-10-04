e4f77b3894b0ea21df0cd1e340847513056bc1bc
Mage04 <mahageetha04@gmail.com>
2020-10-04 13:54:07 +0530

    Add files via upload

---
+++ ResumeWindows/ResumeJS.js
@@ -0,0 +1,70 @@
+
+//var FirstImg=document.createElement("img");
+//FirstImg.src="Image";
+//var FirstSource=document.getElementById("image");
+//FirstSource.appendChild(FirstImg);
+
+
+
+console.log(window.document);
+
+
+
+const Second=document.getElementById("second");
+console.log(Second);
+
+const H1Tag=document.createElement("h1");
+H1Tag.innerHTML="";
+console.log(H1Tag);
+H1Tag.classList.add("naming");
+
+//Second.appendChild(H1Tag);
+
+const Third= document.getElementById("third");
+console.log(Third);
+
+
+const FirstImg=document.querySelector("#image");
+console.log(FirstImg);
+
+const FirstPara=document.querySelector("p.info");
+console.log(FirstPara);
+const HoriLine=document.querySelector("hr");
+console.log(HoriLine);
+const Line1= document.getElementById("line");
+console.log(Line1);
+const Main1=document.querySelector("#h1");
+console.log(Main1);
+const P1=document.querySelector("#p1");
+console.log(P1);
+const H2=document.querySelector("#h2");
+console.log(H2);
+const P2=document.querySelector("#p2");
+console.log(P2);
+const AllDiv= document.querySelectorAll("div");
+console.log(AllDiv);
+
+//const Main=document.getElementById("main");
+//console.log(Main);
+
+//const H1Tag2=document.createElement("h1");
+//H1Tag2.innerText="Profile";
+//console.log(H1Tag2);
+//const Para2=document.createElement("p");
+
+//console.log(Para2);
+//Main1.appendChild(H1Tag2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
